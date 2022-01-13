246628913c96448dd8e1e14f544741cb6ac2e9a2
Ian Hollander <ianholla@gmail.com>
2022-01-13 14:18:59 -0500

    Formatting

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -6,19 +6,19 @@ import App from "../App";
 test("checkbox is initially unchecked", () => {
   render(<App />);
 
-  const addPepperoni = screen.getByRole("checkbox", {name: /add pepperoni/i});
-  
+  const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+
   expect(addPepperoni).not.toBeChecked();
-})
+});
 
 test("checkbox appears as checked when user clicks it", () => {
   render(<App />);
 
-  const addPepperoni = screen.getByRole("checkbox", {name: /add pepperoni/i});
-  
+  const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+
   userEvent.click(addPepperoni);
   expect(addPepperoni).toBeChecked();
-})
+});
 
 test("checkbox appears as unchecked when user clicks a second time", () => {
   render(<App />);
@@ -32,7 +32,6 @@ test("checkbox appears as unchecked when user clicks a second time", () => {
   userEvent.click(addPepperoni);
 
   expect(addPepperoni).not.toBeChecked();
-
 });
 
 // Size select element
