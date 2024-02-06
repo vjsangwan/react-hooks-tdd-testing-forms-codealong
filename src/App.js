0b5022b7f31e92e0f1c87a4a733dfdc5b179084c
a0k03l3 <ashok.sangwan@icanbwell.com>
2024-02-06 12:19:22 -0800

    tdd done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import { useState } from "react";
 
 function App() {
   const [pepperoniIsChecked, setPepperoniIsChecked] = useState(false);
-
+  const [pizzaSize, setPizzaSize] = useState("small");
   const togglePepperoni = (e) => setPepperoniIsChecked(e.target.checked);
+  const selectSizeHandle = (e) => setPizzaSize(e.target.value);
+  const [contactInfo, setContactInfo] = useState("");
+  const updateContactInfo = (e) => setContactInfo(e.target.value);
+  const [messege, setMessage] = useState(false);
+
+  const OnSubmitHandle = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
 
   return (
     <div>
       <h1>Place an Order</h1>
-      <form>
+      <p>
+        Your Selection {pizzaSize} {pepperoniIsChecked ? "pepperoni" : "cheese"}
+      </p>
+      <form onSubmit={OnSubmitHandle}>
         <div>
           <h3>Toppings</h3>
           <input
@@ -20,7 +32,33 @@ function App() {
           />
           <label htmlFor="pepperoni">Add pepperoni</label>
         </div>
+        <div>
+          <h3>Size</h3>
+          <label htmlFor="select-size">Select Size</label>
+          <select
+            id="select-size"
+            value={pizzaSize}
+            onChange={selectSizeHandle}
+          >
+            <option>small</option>
+            <option>medium</option>
+            <option>large</option>
+          </select>
+        </div>
+        <div>
+          <h3>Contact Info</h3>
+          <label htmlFor="email"> Enter your email address :</label>
+          <input
+            type="text"
+            id="email"
+            placeholder="email address"
+            value={contactInfo}
+            onChange={updateContactInfo}
+          />
+        </div>
+        <button type="submit">Submit Order</button>
       </form>
+      {messege ? <h2>"Thank you for the order!" </h2> : null}
     </div>
   );
 }
